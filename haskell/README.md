4d3b40752ffc065fb2c25ddc4bb4681b3426b9e6
Jesse Williamson <Jesse@thunklifes-puter.local>
2018-09-25 16:17:20 -0700

    move to hackyll

--- haskell/README.md
+++
@@ -1,4 +0,0 @@
-LearnMeAHaskell
-===============
-
-Notes and code snippets I wrote while reading Learn You a Haskell...
--- haskell/code/ch1/boomBang.hs
+++
@@ -1,2 +0,0 @@
---for odd numbers less than 10, return boom, otherwise return bang
-boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
